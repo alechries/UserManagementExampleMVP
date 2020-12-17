82a9d2edd9fd6e9e135bfb900b83fc1a53dc2d4a
Alexander <51765464+alechries@users.noreply.github.com>
2020-12-17 20:23:18 +0200

    Create classes

---
+++ UserPresenter.py
@@ -0,0 +1,8 @@
+from UserModule import UserModule
+from UserView import UserView
+
+
+class UserPresenter:
+    def __init__(self, module: UserModule, view: UserView):
+        self.module = module
+        self.view = view

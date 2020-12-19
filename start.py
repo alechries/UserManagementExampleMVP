e872ea28440358323d9c047a8c6c39c3ad3ee598
Alexander <51765464+alechries@users.noreply.github.com>
2020-12-19 13:26:43 +0200

    # test hello

--- start.py
+++ start.py
@@ -3,5 +3,7 @@ from MVP.UserView import UserView
 from MVP.UserPresenter import UserPresenter
 
 if __name__ == '__main__':
-    # Start on initialization +++
+    # Start on initialization
+    # Test
+    # Hello
     presenter = UserPresenter()
